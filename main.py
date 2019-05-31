ff4b769c93b1e51647604162af489f8695bbcb8b
Vyacheslav <48791104+uniquepeero@users.noreply.github.com>
2019-05-31 18:33:37 +0500

    some features

--- main.py
+++ main.py
@@ -47,22 +47,25 @@ class Pinnacle:
 			if index['name'] == 'Tennis':
 				return index['id']
 
-	def lines_fixtures(self, sportId):
+	def lines_fixtures(self, sportId, since=None):
 		headers = {
 			'Accept': 'application/json',
 			'Authorization': self.AUTH
 		}
+		if since is None:
+			since = ''
+		else:
+			since = f'&since={since}'
 		try:
-			res = requests.get(f'{self.URL}/v1/fixtures?sportId={sportId}', headers=headers)
+			res = requests.get(f'{self.URL}/v1/fixtures?sportId={sportId}{since}', headers=headers)
 			if res.status_code == requests.codes.ok:
-				log.debug(f'Lines Fixtures:\n{res.json()}')
 				return res.json()
 			else:
 				log.error(f'Lines Fixtures request: Code {res.status_code} / {res.json()}')
 		except Exception:
 			log.error(f'Get Lines Fixtures: ', exc_info=True)
 
-	def lines_odds(self, sportId, eventIds=None):
+	def lines_odds(self, sportId, since=None, eventIds=None):
 		headers = {
 			'Accept': 'application/json',
 			'Authorization': self.AUTH
@@ -72,15 +75,18 @@ class Pinnacle:
 		else:
 			# Сюда добавить список
 			events = f'&eventIds={eventIds}'
+		if since is None:
+			since = ''
+		else:
+			since = f'&since={since}'
 		try:
-			res = requests.get(f'{self.URL}/v1/odds/parlay?sportId={sportId}&oddsFormat=Decimal{events}', headers=headers)
+			res = requests.get(f'{self.URL}/v1/odds?sportId={sportId}&oddsFormat=Decimal{events}{since}', headers=headers)
 			if res.status_code == requests.codes.ok:
-				log.debug(f'Lines Parlay Odds:\n{res.json()}')
 				return res.json()
 			else:
 				log.error(f'Lines Parlay Odds request: Code {res.status_code} / {res.json()}')
 		except Exception:
-			log.error(f'Get Lines Parlay Odds: ', exc_info=True)
+			log.critical(f'Get Lines Parlay Odds: ', exc_info=True)
 
 	def client_balance(self):
 		headers = {
@@ -90,7 +96,6 @@ class Pinnacle:
 		try:
 			res = requests.get(f'{self.URL}/v1/client/balance', headers=headers)
 			if res.status_code == requests.codes.ok:
-				log.debug(f'Client Balance:\n{res.json()}')
 				return res.json()
 			else:
 				log.error(f'Client balance request: Code {res.status_code} / {res.json()}')
@@ -101,9 +106,32 @@ if __name__ == '__main__':
 	try:
 		log.info('Started')
 		pin = Pinnacle()
-
-		#log.debug(pin.find_sport_tennis(pin.lines_sports()))
 		tennis = 33
+		firstline = pin.lines_fixtures(tennis)
+		last = firstline['last']
+		alexline = {
+			'id1': {
+				'p1': 'Sloane Stephens',
+				'p2': 'Polona Hercog',
+				'p1_odds': 1.2,
+				'p2_odds': 3.8,
+				'isfound': False
+			},
+			'id2': {
+				'p1': 'Belinda Bencic',
+				'p2': 'Donna Vekic',
+				'p1_odds': 1.8,
+				'p2_odds': 2.1,
+				'isfound': False
+			}
+		}
+		for key, value in alexline.items():
+			log.debug(f'AL Key: {key}')
+			log.debug(f'AL Value: {value}')
+			if value['p1'] and value['p2'] in firstline.items():
+				log.info(f'FOUND {value["p1"]} and {value["p2"]} in firstline')
+
+
 		line = pin.lines_fixtures(tennis)
 		odds = pin.lines_odds(tennis)
 
